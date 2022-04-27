ced72faf1a5b3d12cf952a9f37e100f3f3e269be
Mast_mex <mastmex42@gmail.com>
2022-04-27 22:52:18 +0300

    doxygen

--- source/classes/win/win.cpp
+++ source/classes/win/win.cpp
@@ -3,18 +3,25 @@ win::win()
 {
     
 }
+/**
+ * @brief Отрисовка окна
+ * @author Mast_mex
+ * @version 0.1
+ * 
+ * @details Данный клас служит для отрисовки окна. В Нем устанавливаются все основные значения.
+ */
 void win::start(){
     sf::RenderWindow window(sf::VideoMode(600,480),"TBDI2D",sf::Style::Default);
-    window.setFramerateLimit(60);
+    window.setFramerateLimit(60); 
     while (window.isOpen())
     {
-        // check all the window's events that were triggered since the last iteration of the loop
+        /// @details check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
         {
-            // "close requested" event: we close the window
+            /// @details "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
-                window.close();
+                window.close(); 
         if (event.type == sf::Event::Resized)
                 {
                     sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
@@ -22,7 +29,7 @@ void win::start(){
                 }
         }
 
-        // clear the window with black color
+        /// @details clear the window with black color
         window.clear(sf::Color::Black);
         window.display();
     }
